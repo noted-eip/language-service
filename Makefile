65eb42f2eac3eb87a1dea3bd0ef04eb929203596
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2022-05-21 16:08:00 +0200

    feat(compilation): codegen path fix

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 # Run the protoc compiler to generate the Golang server code.
 codegen: update-submodules
+	echo `pwd`
 	docker run --rm -v `pwd`/grpc:/app/grpc -v `pwd`/misc:/app/misc -w /app noted-python-protoc /bin/sh -c misc/gen_proto.sh
 
 # Fetch the latest version of the protos submodule.
--- misc/gen_proto.sh
+++ misc/gen_proto.sh
@@ -1,2 +1,2 @@
 rm -rf grpc/*pb/
-python -m grpc_tools.protoc --python_out=. --grpc_python_out=. grpc/protos/recommendations/*.proto --proto_path=./grpc/protos
+python -m grpc_tools.protoc --python_out=./grpc --grpc_python_out=./grpc grpc/protos/recommendations/*.proto --proto_path=./grpc/protos
