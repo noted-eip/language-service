25eb78a5134078a00b77a266a73c665343f51552
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2022-05-22 22:46:02 +0200

    fix(gen_proto.sh): missing file in commit 16de85231b22f523ed4430b494068f0a197593e5
    Commit message:
    feat(gen_proto.sh): better genericity (variables) + explicit comments
                      : change auto names to Noted's convention names
                      : symbolic link to hotfix protobufs python's imports

--- misc/gen_proto.sh
+++ misc/gen_proto.sh
@@ -1,2 +1,25 @@
-rm -rf grpc/*pb/
-python -m grpc_tools.protoc --python_out=./grpc --grpc_python_out=./grpc grpc/protos/recommendations/*.proto --proto_path=./grpc/protos
+# Variable definition and init
+SERVICE_NAME="recommendations"
+PROTOC_TOOL="python -m grpc_tools.protoc" # Could be hardcoded in CMD later on
+GRPC_FOLDER="./grpc"
+
+(
+# Clean up old files
+rm -rf "${GRPC_FOLDER}/${SERVICE_NAME}pb" "${SERVICE_NAME}pb" && \
+
+# Generate protobufs files in the right folder
+$PROTOC_TOOL --python_out="${GRPC_FOLDER}" --grpc_python_out="${GRPC_FOLDER}" "${GRPC_FOLDER#./}/protos/${SERVICE_NAME}/"*.proto --proto_path="${GRPC_FOLDER}/protos" && \
+
+# Create __init__.py to easily import python files
+touch "${GRPC_FOLDER}/${SERVICE_NAME}/__init__.py" && \
+
+# Rename the folder to keep Noted's convention
+mv "${GRPC_FOLDER}/${SERVICE_NAME}" "${GRPC_FOLDER}/${SERVICE_NAME}pb" && \
+
+# Create symbolic link for easier import in Python
+ln -s "${GRPC_FOLDER}/${SERVICE_NAME}pb" "${SERVICE_NAME}pb"
+
+# Hotfix for renaming python generated package
+sed -i "s/from ${SERVICE_NAME}/from ${SERVICE_NAME}pb/g" "${SERVICE_NAME}pb/${SERVICE_NAME}_pb2_grpc.py"
+) \
+|| echo -ne "\nGenerating protobuf files failed."
