7febd387e67181f405d58195bbd684258070b61d
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2022-05-27 17:15:58 +0200

    feat(recommendations-service.py): better logging, and quick ctor+env fixes

--- recommendations-service.py
+++ recommendations-service.py
@@ -6,6 +6,9 @@ import grpc
 import recommendationspb.recommendations_pb2_grpc as pb2_grpc
 import recommendationspb.recommendations_pb2 as pb2
 
+# Logger
+from loguru import logger
+
 # Keyphrase extraction framework
 import pke
 from pke.lang import stopwords 
@@ -14,40 +17,50 @@ import os # getenv
 
 
 """
-
+Long term, we will have multiple algorithm to choose from
 """
 
 class RecommendationsService(pb2_grpc.RecommendationsServiceServicer):
-    extractor = pke.unsupervised.YAKE()
 
-    lang                = os.getenv("LANG")                or 'fr'
-    number_of_results   = os.getenv("NUMBER_OF_KEYWORDS")  or 5
-    n_gram_length       = os.getenv("N_GRAM_LENGTH")       or 2 # between 1 and 3
-    co_occurence_window = os.getenv("CO_OCCURENCE_WINDOW") or 3 
-    threshold           = os.getenv("THRESHOLD")           or 0.75 
+    extractor           = pke.unsupervised.YAKE()
+
 
     def __init__(self, *args, **kwargs):
+        self.lang                =       os.getenv("AI_LANG")                 or 'fr'
+        self.number_of_results   = int  (os.getenv("AI_NUMBER_OF_KEYWORDS"))  or 5
+        self.n_gram_length       = int  (os.getenv("AI_N_GRAM_LENGTH"))       or 2 # between 1 and 3
+        self.co_occurence_window = int  (os.getenv("AI_CO_OCCURENCE_WINDOW")) or 3 
+        self.threshold           = float(os.getenv("AI_THRESHOLD"))           or 0.75 
+
+        logger.info("Recommendation service has been successfully initialized")
+
+    def __candidate_selection_and_weighting(self):
+        logger.debug("Candidate selection and weighting being processed")
         self.extractor.candidate_selection(n=self.n_gram_length)
-        self.extractor.candidate_weighting(window=self.co_occurence_window)
-        print("Service initialized ✅")
+        self.extractor.candidate_weighting(window=self.co_occurence_window, use_stems=False)
 
     def ExtractKeywords(self, request, context):
-        extractor.load_document(input=request.content,
-                                language=self.lang,
-                                stoplist=stopwords[lang],
-                                normalization=None)
+        logger.info("ExtractKeywords request being processed")
 
-        # Will be represented with a list of tuple as such : [ ("keyword", keyword_score), ... ]
-        keywords_verbose = extractor.get_n_best(n=self.number_of_results, threshold=self.threshold)
+        self.extractor.load_document(input=request.content,
+                                     language=self.lang,
+                                     stoplist=stopwords[self.lang],
+                                     normalization=None)
+        self.__candidate_selection_and_weighting()
 
-        keywords = [keyword[0] for keyword in keywords_verbose]
+        keywords_verbose = self.extractor.get_n_best(n=self.number_of_results, threshold=self.threshold)
+
+        keywords = [keyword_info[0] for keyword_info in keywords_verbose]
 
         result = {'keywords': keywords}
+
+        logger.debug(f"Response: {keywords}")
+        logger.info("ExtractKeywords response being sent")
 
         return pb2.ExtractKeywordsReply(**result)
 
 # TODO: utils.py or utils/env.py
-def get_env_variable(name: str) -> str:
+def get_required_env_variable(name: str) -> str:
     result = os.getenv(name);
     if result is None:
         raise EnvironmentError(f"Please set the {name} env variable")
@@ -55,13 +68,15 @@ def get_env_variable(name: str) -> str:
 
 # TODO: server.py or main.py
 def serve():
-    port = get_env_variable("PORT");
+    port = get_required_env_variable("PORT");
     max_workers = os.getenv("MAX_WORKERS") or 8;
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=max_workers))
     
     pb2_grpc.add_RecommendationsServiceServicer_to_server(RecommendationsService(), server)
 
+    logger.info(f"Opening server on port {port}")
+    
     server.add_insecure_port(f'[::]:{port}')
     server.start()
     server.wait_for_termination()
