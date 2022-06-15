df3168aa09b2d75feba3feed63491a422810962a
Edouard SENGEISSEN <edouard.sengeissen@epitech.eu>
2022-06-15 13:43:14 +0200

    fix(logger): removed comment

--- custom_logger.py
+++ custom_logger.py
@@ -1,8 +1,6 @@
 from loguru import logger
 import json
 
-# For later sprints, a sink could/should be this https://docs.python.org/3/glossary.html#term-file-object
-
 def __serialize(record):
     subset = {"level": record["level"].name.lower(), "ts": record["time"].timestamp(), "msg": record["message"], **record["extra"]}
     return json.dumps(subset)
